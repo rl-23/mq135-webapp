cd51338e26fd3b1b429daafa943e0ba7e3f323ec
rl-23 <69859315+rl-23@users.noreply.github.com>
2021-04-30 11:49:59 +0100

    Update servermain.py

--- ADUINO_WITH_MQ135/servermain.py
+++ ADUINO_WITH_MQ135/servermain.py
@@ -1,9 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route("/")
+def homepage():
+    return """
+    <!DOCTYPE">
+ <html>
+   <head>
+      <title> Indoor Air Quality</title>
+   </head>
+   <body>
+   <p>hi</p>
+   </body>
+</html>
+    """
 
-if __name__ == "__main__":
-	app.run()
+if __name__ == '__main__':
+    app.run(use_reloader=True, debug=True)
