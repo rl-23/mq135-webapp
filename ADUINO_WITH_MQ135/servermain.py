d7482a1baf3c49abc7fb96c10ebbc04d6d0fee6f
rl-23 <69859315+rl-23@users.noreply.github.com>
2021-04-30 12:01:02 +0100

    Update servermain.py

--- ADUINO_WITH_MQ135/servermain.py
+++ ADUINO_WITH_MQ135/servermain.py
@@ -4,7 +4,7 @@ app = Flask('testapp')
 
 @app.route('/')
 def index():
-    return render_template('index.html', appdata='test')
+    return render_template('index.html', appdata = 'test', airmine = 'test' )
 
 if __name__ == '__main__':
     app.run()
